fc7ecf6d4c1a56daacfb57128c1e13a9c22cf2cb
audrbar <audrbar@gmail.com>
2024-06-29 23:24:46 +0300

    Upd: lottie

--- src/app.py
+++ src/app.py
@@ -8,13 +8,6 @@ import sys
 # Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Data Science App", page_icon=":🌐:", layout="wide")
 
-# Download your emoji
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
 # Use local CSS
 def local_css(file_name):
     with open(file_name) as f:
